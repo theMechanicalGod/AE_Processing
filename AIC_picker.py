22fd167c8b574a7ce78e99ad4cf1159503ee4f41
theMechanicalGod <caelinmuir111@gmail.com>
2020-03-01 12:28:20 -0800

    Add files via upload

--- AIC_picker.py
+++ AIC_picker.py
@@ -1,23 +1,6 @@
-'''
-Author: Caelin Muir
-Contact: muir@ucsb.edu
-Version: 200301
-
-Library for AIC method of finding arrival time of acoustic wave. Developed
-based on:
-
-Sedlak, P., Hirose, Y., & Enoki, M. (2013). Acoustic emission localization in
-thin multi-layer plates using first-arrival determination. Mechanical Systems
-and Signal Processing, 36(2), 636–649
-'''
-
-
-
-from scipy import signal
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas
-from scipy.stats import linregress
+
+
 
 def Sedlak_CF(volt, R = 4):
     '''
